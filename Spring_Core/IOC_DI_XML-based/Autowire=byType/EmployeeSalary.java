6938c93ee59d838329f66ec130770ea11731cabe
BhanuPrasadKommula <bhanuprasadkommula98@gmail.com>
2025-04-10 01:41:33 -0500

    Add files via upload

---
+++ Spring_Core/IOC_DI_XML-based/Autowire=byType/EmployeeSalary.java
@@ -0,0 +1,14 @@
+package com.myproject.java_spring;
+
+public class EmployeeSalary {
+    private double salary;
+
+    public EmployeeSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+}
